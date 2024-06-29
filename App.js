c5eb7c84a42d8e6742881d04bfcda30ac1984fd7
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-06-29 12:37:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
      <Nav/>
-     <img  width={'100%'}  src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg'/>
+     <img  width={'100%'} position={'relative'} top={'0px'} left={'0px'} src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg'/>
 
      <Featured/>
     </div>
