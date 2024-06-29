4f84d33ac4455dfd0068fb5a8dd8fb20ea477571
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-06-29 12:22:26 +0530

    Add files via upload

---
+++ Featured.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import st from './Featured.module.css'
+
+const Featured = () => {
+  return (
+    <div className={st.play}>
+        <img src="https://cdn11.bigcommerce.com/s-mmafe1g4n3/images/stencil/1280x1280/products/7991/23023/MM00267456__62419.1698746940.jpg?c=1" alt="" />
+    </div>
+  )
+}
+
+export default Featured
