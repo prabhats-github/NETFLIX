08f0f49956a37b39155fafe0a8130b95fce34d6f
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-06-29 12:22:03 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './componants/Nav';
+import Featured from './componants/Featured';
 
 function App() {
   return (
     <div className="App">
      <Nav/>
      <img  width={'100%'}  src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg'/>
+
+     <Featured/>
     </div>
   );
 }
